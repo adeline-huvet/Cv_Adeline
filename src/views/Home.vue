f9a0a878999beb9328018f58ec3c88584cf7553a
geoffrey-gallot <geoffrey.gallot@gmail.com>
2022-01-18 10:31:09 +0100

    rectif css page home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
     <div class="main-container">
-        <h2>Adeline Huvet</h2>
+      <h2>Adeline Huvet</h2>
       <div class="title">
-        <vue-typer class="text"
-          :text="[
-            'Développeuse Web junior, Fullstack.',  
-          ]"
-        
+        <vue-typer
+          class="text"
+          :text="['Développeuse Web junior, Fullstack.']"
           :repeat="Infinity"
           :shuffle="false"
           initial-action="typing"
@@ -19,15 +17,23 @@
           :erase-on-complete="false"
           caret-animation="blink"
         ></vue-typer>
-      
       </div>
-
-      <div class="social-network">
-        <ul class="network-group">
-             <li class="network-item-git"><a href="https://github.com/adeline-huvet"><i class="fab fa-github fa-3x"> </i></a></li>
-            <li class="network-item-link"><a href="https://www.linkedin.com/in/adeline-huvet-a0640b20a/?originalSubdomain=fr"><i class="fab fa-linkedin fa-3x"></i></a></li>
-        </ul>
-      </div>
+    </div>
+    <div class="social-network">
+      <ul class="network-group">
+        <li class="network-item-git">
+          <a href="https://github.com/adeline-huvet">
+            <i class="fab fa-github fa-3x"></i>
+          </a>
+        </li>
+        <li class="network-item-link">
+          <a
+            href="https://www.linkedin.com/in/adeline-huvet-a0640b20a/?originalSubdomain=fr"
+          >
+            <i class="fab fa-linkedin fa-3x"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -36,46 +42,42 @@
 import { VueTyper } from "vue-typer";
 export default {
   name: "Home",
-  components: { 
-      VueTyper
-      },
-     
+  components: {
+    VueTyper
+  }
 };
 </script>
 
 <style scoped>
-
 @media screen and (max-width: 900px) {
-
-  h2{
-   transform: translateY(5.5rem);
-   margin-right: auto;
-   margin-left: 20px;
+  h2 {
+    transform: translateY(5.5rem);
+    margin-right: auto;
+    margin-left: 20px;
   }
-  .title{
+  .title {
     margin-right: auto;
     margin-left: 20px;
     transform: translateY(40px);
   }
-  .vue-typer, .custom.char{
+  .vue-typer,
+  .custom.char {
     font-size: 2px;
-    
   }
-  .social-network{
+  .social-network {
     left: 30%;
     position: absolute;
     bottom: 40px;
   }
 }
-h2{
-  font-family: 'Italiana', serif;
+h2 {
+  font-family: "Italiana", serif;
   font-size: 300%;
   margin-right: 52rem;
   margin-top: -5rem;
-  
 }
 
-.home{
+.home {
   position: fixed;
   height: 100vh;
   width: 100%;
@@ -92,52 +94,43 @@ h2{
   align-items: center;
   justify-content: center;
   margin-top: 15%;
-  
 }
 
 .vue-typer {
-    font-family: 'Rambla', sans-serif;
-    font-size: 3.5rem;
-    
+  font-family: "Rambla", sans-serif;
+  font-size: 3.5rem;
 }
 .text {
-  color: #607D8B;
-  
+  color: #607d8b;
 }
-.title{
+.title {
   margin-top: 70px;
 }
-.network-group{
-    list-style: none;
-    margin-top: 20px;
+.network-group {
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 20px;
 }
-.network-item-git{
-    display: inline-block;
-    margin: 7px;
-    color: #171515;
-    opacity: 70%;
-    transition: all 0.5s ease-out;    
+.network-item-git {
+  margin: 7px;
+  margin-right: 15px;
+  color: #171515;
+  transition: all 0.5s ease-out;
 }
-.network-item-link{
-   display: inline-block;
-    margin: 7px;
-    color:  #0e76a8;
-    opacity: 70%;
-    transition: all 1s ease-out;
+.network-item-link {
+  margin: 7px;
+  margin-left: 15px;
+  color: #0e76a8;
+  transition: all 0.5s ease-out;
 }
-.network-item-git:hover{
-  display: inline-block;
-    margin-right: 35px;
-    color:  #dbdbdb;
-    transform:scale(2,2);
-    transition: all 1s ease-in;
-} 
-.network-item-link:hover{
-  display: inline-block;
-    margin-left: 35px;
-    color:  #dbdbdb;
-    transform:scale(2,2);
-    transition: all 1s ease-in;
+.network-item-git:hover {
+  transform: scale(2, 2);
+  transition: all 0.5s ease-in;
 }
-
+.network-item-link:hover {
+  transform: scale(2, 2);
+  transition: all 0.5s ease-in;
+}
 </style>
